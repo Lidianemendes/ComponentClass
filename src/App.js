5872258138c1fbf951669e599c5b1965c67d3d74
Lidianemendes <93939970+Lidianemendes@users.noreply.github.com>
2022-02-02 23:05:13 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Ola extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Vai na Web</h1>
+        <p>Desenvolvedora Front-end dando os primeiros passos com ReactJS.</p>
+      </div>
+    );
+  }
+}
+
+export default Ola;
